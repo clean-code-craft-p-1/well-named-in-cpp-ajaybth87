3edefe470a2b767b20fc6aa66fc84909b742a5a0
Ajay Kumar <ajay.kumar_9@philips.com>
2023-07-31 19:07:00 +0530

    Error Fix

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ int main() {
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
-    printColorCodeReference();
+    ShowColorCodeReference();
 
     return 0;
 }
